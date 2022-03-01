2f0b48c4ec3389439ab376c52c1270ccbce5a3a7
AkemiHomura666 <73525819+AkemiHomura666@users.noreply.github.com>
2022-03-01 16:44:31 +0100

    Update mipslabwork.c

--- time4int/mipslabwork.c
+++ time4int/mipslabwork.c
@@ -11,27 +11,18 @@
    For copyright and licensing, see file COPYING */
 
 #include <stdint.h>   /* Declarations of uint_32 and the like */
-#include <pic32mx.h>  /* Declarations of system-specific addresses etc */
+#include "pic32mx.h"  /* Declarations of system-specific addresses etc */
 #include "mipslab.h"  /* Declatations for these labs */
 
-int prime = 1234567;
 int counttimer = 0;
 int mytime = 0x5957;
 
 char textstring[] = "text, more text, and even more text!";
 
 /* Interrupt Service Routine */
-void user_isr( void ) {
-  counttimer++;
-  if (counttimer == 10){
-    time2string( textstring, mytime );
-  display_string( 3, textstring );
-  display_update();
-  tick( &mytime );
-  counttimer = 0;
-  }
-  IFSCLR(0)=0x100;
-  IECSET(0) = 0x00000100;
+void user_isr( void )
+{
+  return;
 }
 
 /* Lab-specific initialization goes here */
@@ -46,19 +37,44 @@ void labinit( void )
   T2CONSET = 0x70;    // Set prescale to 1:256
   TMR2 = 0x0;         // Clear timer register
   PR2 = 0x7a12;       // Load period register
+  IFSCLR(0)=0x100;
   T2CONSET = 0x8000;
-
-  IFSCLR(0)=0x100;    // Clear timer interrupt register
-  // IPCSET(2) = 0x0000000C;
-  // IPCSET(2) = 0x00000001;
-  IPCSET(2) = 0x0000000D;
-  IECSET(0) = 0x100;
   return;
 }
 
 /* This function is called repetitively from the main program */
-void labwork( void ) {
-  prime = nextprime( prime );
-  display_string( 0, itoaconv( prime ) );
-  display_update();
+void labwork( void )
+{
+  volatile int *porte = (volatile int*) 0xbf886110;
+  volatile int *LED = (volatile int*) 0xbf886110; 
+  //delay( 1000 );
+
+  //getbtn code
+  int sw = getsw();
+
+  if ((getbtns() >> 2) & 1){
+      mytime = ((mytime & 4095) | (sw << 12));
+  }
+   if ((getbtns() & 2) == 2){
+      mytime = ((mytime & 61695) | (sw << 8));
+  }
+    if ((getbtns() & 1) == 1){
+      mytime = ((mytime & 65295) | (sw << 4));
+  }
+
+if ((IFS(0) & 0x100)){
+time2string( textstring, mytime );
+display_string( 3, textstring );
+display_update();
+display_image(96, icon);
+counttimer++;
+IFSCLR(0) = 0x100;
+}
+
+if (counttimer == 10){
+
+  tick( &mytime );
+  *LED = *LED + 1;
+  counttimer = 0;
+}
 }
