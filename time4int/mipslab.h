1b6609940810ddc710201f28daafdddb2f581069
mittuskaka <89848199+mittuskaka@users.noreply.github.com>
2022-03-02 22:04:56 +0100

    fixed type

--- time4int/mipslab.h
+++ time4int/mipslab.h
@@ -12,7 +12,7 @@ void display_image(int x, const uint8_t *data);
 void display_init(void);
 void display_string(int line, char *s);
 void display_update(void);
-uint8_t spi_send_recv(uint8_t data);
+void spi_send_recv(uint8_t data);
 
 /* Declare lab-related functions from mipslabfunc.c */
 char * itoaconv( int num );
