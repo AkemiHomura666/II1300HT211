e81b7648263b222bf46a204b03c53dac621add06
LiuDongLin <liudonglin1221@gmail.com>
2022-03-03 23:33:25 +0100

    ball

--- ball.c
+++ ball.c
@@ -2,6 +2,24 @@
 #include <pic32mx.h>  /* Declarations of system-specific addresses etc */
 #include "mipslab.h"  /* Declatations for these labs */
 
-
+int ball_speed_x = 30;
+int ball_speed_y = 10;
 void ball(int x, int y){
+    display_pixel(x,y);
+    display_pixel(x+1,y);
+    display_pixel(x,y+1);
+    display_pixel(x-1,y);
+    display_pixel(x,y-1);
+}
+
+void horizontal_direction_change(int x){
+    ball_speed_x = -1 * ball_speed_x;
+}
+
+void verticle_direction_change(int x){
+    ball_speed_y = -1 * ball_speed_y;
+}
+
+void ball_hit_paddle(int x, int y){
+    
 }
