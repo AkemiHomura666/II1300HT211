6da4b774e99b10670e7042a80ab8cba5dd19c6c6
mittuskaka <89848199+mittuskaka@users.noreply.github.com>
2022-03-03 15:27:01 +0100

    movepaddle

--- time4int/updatePonging.c
+++ time4int/updatePonging.c
@@ -2,6 +2,23 @@
 #include <pic32mx.h>  /* Declarations of system-specific addresses etc */
 #include "mipslab.h"  /* Declatations for these labs */
 
+//activated  by whataboutbuttons to move paddle up or down
+void movePaddle(int a){
+  if (a == 0){
+    //code for moving paddle up
+    p1 = p1-1;
+  }
+    else if (a == 1){
+        //code for moving paddle down  
+        p1 = p1+1;
+    }
+    else if (a == 2){
+        p2 = p2-1;
+        }
+    else if (a == 3){
+        p2 = p2+1;
+  }
+
 void updatePonging(void){
 			whataboutbuttons();
 			whataboutintersect();
