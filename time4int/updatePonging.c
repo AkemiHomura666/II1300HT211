3575d43a8a289c97072bf2ed451b0eea52790c24
mittuskaka <89848199+mittuskaka@users.noreply.github.com>
2022-03-04 12:24:40 +0100

    merger

--- time4int/updatePonging.c
+++ time4int/updatePonging.c
@@ -71,16 +71,17 @@ void updatePonging(void){
 
 void whataboutintersect(void){
     // kod för direction change på paddle
-    check_hit();
+
     // kod för direction change på top / bottom
-    game_over();
+
     //kod för goaaal
+    goal(0);
     return;
 }
 
 void whataboutball(void){
     // kod för ball position
-    ball_moving();
+
     // kod för ball movement
 
     //
@@ -89,8 +90,6 @@ void whataboutball(void){
 
 void goal(int scorer){
     score[scorer]++;
-    display_hex(32, 2, score[0]+16);
-    display_hex((95), 2, score[0]+16);
 }
 
 void resetponging(void){
