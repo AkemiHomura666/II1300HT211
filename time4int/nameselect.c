5e8566e69c6c1d2819a68d048b7069ad513b46ee
LiuDongLin <liudonglin1221@gmail.com>
2022-03-06 16:58:47 +0100

    name

--- time4int/nameselect.c
+++ time4int/nameselect.c
@@ -1,7 +1,16 @@
 #include <stdint.h>   /* Declarations of uint_32 and the like */
 #include "pic32mx.h"  /* Declarations of system-specific addresses etc */
 #include "mipslab.h"  /* Declatations for these labs */
+#include "fontheader.h"
 
 void nameselect(int selection){
-    
+    if (selection == 0){
+
+    }
+    if (selection == 1){
+
+    }
+    if (selection == 0){
+
+    }
 }
