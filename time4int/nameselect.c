99948f0f240959b59ed26f71b178e7b20501c4ad
mittuskaka <89848199+mittuskaka@users.noreply.github.com>
2022-03-07 14:31:53 +0100

    ok

--- time4int/nameselect.c
+++ time4int/nameselect.c
@@ -3,7 +3,8 @@
 #include "mipslab.h"  /* Declatations for these labs */
 #include "fontheader.h"
 void nameselect(int selection){
-    char name[26]={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+    //char name[26]={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+    char name[26]= "ABCDEFGHIJKLMNOPQRSTUVXYZ";
     int i;
     int j;
     reset_string();
